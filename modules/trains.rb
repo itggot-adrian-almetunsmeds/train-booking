7a34dbcf8be2ed01af9561d639d35721e9b1b969
itggot-adrian-almetunsmeds <adrian.almetunsmeds@elev.ga.ntig.se>
2019-11-14 16:56:24 +0100

    :tada: Added basic train functionality

--- modules/trains.rb
+++ modules/trains.rb
@@ -1,13 +1,40 @@
 # frozen_string_literal: true
 
 require_relative 'db_handler'
+# Handles trains
+class Train < DBHandler
+  attr_reader :status, :main_location, :type_id, :id
+  attr_writer :status, :main_location, :type_id
 
-class Trains
-  def self.initialize; end
+  # Creates a new train
+  def initialize(id = nil)
+    if id.nil?
+      @status = 'operational'
+      @main_location = "G\xC3\xB6teborg C"
+      @type_id = 0
+      @id = (DBHandler.write_to_db 'trains', self)['id']
+    else
+      temp = DBHandler.with_id('trains', id)
+      @status = temp['status']
+      @main_location = temp['main_location']
+      @type_id = temp['type_id']
+      @id = temp['id']
+    end
+  end
 
+  # Retrives data from trains table
+  #
+  # Returns data from trains table as hash
   def self.all
-    DBHandler.execute('SELECT * FROM trains')
+    super('trains')
+  end
+
+  def save
+    super('trains', self)
   end
 end
 
-Trains.all
+x = Train.new(34)
+p x
+x.type_id = 5
+x.save
