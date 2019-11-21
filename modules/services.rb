7cefdaa77468e5678bc5233fb63a81a2fa5f1b61
itggot-adrian-almetunsmeds <adrian.almetunsmeds@elev.ga.ntig.se>
2019-11-21 16:46:38 +0100

    :sparkles: Added search functionallity

--- modules/services.rb
+++ modules/services.rb
@@ -7,23 +7,31 @@ class Service < DBHandler
     super('services')
   end
 
+  def self.with_id(id)
+    super('services', id)
+  end
+
   def self.search(params)
     params['dep'] = '%' + params['dep'] + '%'
     params['arr'] = '%' + params['arr'] + '%'
-    params['date'] = Time.parse(params['date']).to_time.to_i
+    params['time'] = DateTime.parse(params['time']).to_time.to_i
     dep = DBHandler.execute('SELECT * FROM destinations WHERE name LIKE ?', params['dep'])
     arr = DBHandler.execute('SELECT * FROM destinations WHERE name LIKE ?', params['arr'])
-    x = DBHandler.execute('SELECT * FROM services WHERE departure_id = ? AND arrival_id = ? AND departure_time > ?', [dep.first['id'], arr.first['id'], params['date']])
-    p x
-    if x.is_a? Array
+    x = DBHandler.execute('SELECT * FROM services WHERE departure_id = ? AND arrival_id = ? AND departure_time > ?', [dep.first['id'], arr.first['id'], params['time']])
+    if x.empty?
+      return 'No such data'
+    elsif x.is_a? Array
       x.first['departure'] = dep.first['name']
       x.first['arrival'] = arr.first['name']
+      return x
     else
       x['departure'] = dep.first['name']
       x['arrival'] = arr.first['name']
+      return x
     end
-    x
   end
 
-  def self.filter_dep_time(time); end
+  def self.tickets(id)
+    x = DBHandler.execute('SELECT * FROM connector JOIN tickets ON tickets.id = connector.ticket_id WHERE connector.service_id = ?', id)
+  end
 end
