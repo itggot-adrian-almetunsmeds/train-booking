a1335007b2454c9e5e0ddda3787078b9be960166
itggot-adrian-almetunsmeds <adrian.almetunsmeds@elev.ga.ntig.se>
2020-01-23 10:55:48 +0100

    Implemented ORM

--- modules/seats.rb
+++ modules/seats.rb
@@ -9,111 +9,51 @@ class Objects
   end
 end
 
-class SeatConnector < DBHandler
+class Seat_connector < DBHandler
   set_table :seat_connector
   set_columns :seat_id, :service_id, :booking_id, :ticket_id
 end
 
 class Seat < DBHandler
   set_table :seat
-  set_columns :id, :service_id, :occupoied, :booking_id
-  has_a :service
-
-  def initialize(things)
-    things.each do |k|
-      k.each do |s, v|
-        if !instance_variable_get("@#{s}").nil?
-          value = instance_variable_get("@#{s}")
-          if value.is_a? Array
-            value << v
-          else
-            value = [value, v] unless value == v
-          end
-          instance_variable_set("@#{s}", value)
-        else
-          instance_variable_set("@#{s}", v)
-        end
-        self.class.send(:attr_reader, s.to_sym)
-      end
-    end
-  end
+  set_columns :id, :service_id, :occupied, :booking_id
+  # has_a :service
 
   def self.booked_seats(booking_id)
     seats_hash = execute 'SELECT * FROM seats_connector WHERE booking_id = ?', booking_id
     seats_array = []
     seats_hash.each do |k|
       seats_array << k
-      p 'This is k'
-      p k
     end
     new seats_array
-  end
-
-  def self.booked_seats_ticket(booking_id, ticket_id)
-    seats_hash = execute 'SELECT * FROM seats_connector WHERE booking_id = ? AND ticket_id = ?', booking_id, ticket_id
-    seats_array = []
-    seats_hash.each do |k|
-      seats_array << k
-    end
-    new seats_array
-  end
-
-  def self.booked_seat(booking_id, service_id)
-    x = execute('SELECT * FROM seats_connector WHERE booking_id = ? AND service_id = ?', booking_id, service_id)
-    # p x
-    new x
   end
 
   def self.fetch(id)
     with_id 'seats', id
   end
 
-  def self.get_empty_seats(service_id)
-    execute 'SELECT * FROM seats WHERE occupied = 0 AND service_id = ?', service_id.to_i
-  end
-
-  def self.get_occupied_seats(service_id)
-    execute 'SELECT * FROM seats WHERE occupied = 1 AND service_id = ?', service_id
-  end
-
-  def self.empty_seats?(_service_id)
-    execute('SELECT COUNT(id) FROM seats WHERE occupied = 0').first ? true : false
-  end
-
   def self.reserve(amount, service_id, boocking_id, ticket_id)
-    seats = Seats.fetch_where service_id: service_id, occupied: 0
-    raise 'Did not select any seats' if seats = []
+    seats = Seat.fetch_where service_id: service_id, occupied: 0
+    raise 'Did not select any seats' if seats == []
 
+    reserved = []
     if seats.is_a? Array
-      raise 'No available seats' if seats.length < amount
+      raise 'No available seats' if seats.length < amount.to_i
 
       seats.each_with_index do |seat, index|
-        next if index >= amount
+        next if index >= amount.to_i
 
         seat.occupied = 1
-        seat.boocking_id = boocking_id
-        temp = SeatConnector.new(seat_id: seat.id, service_id: service_id, booking_id: boocking_id, ticket_id: ticket_id)
+        seat.booking_id = boocking_id
+        temp = Seat_connector.new(seat_id: seat.id, service_id: service_id,
+                                  booking_id: boocking_id, ticket_id: ticket_id)
         temp.save
         seat.save
+        reserved << seat
       end
     end
-    # seats.each_with_index do |seat, i|
-    #   break if i >= amount
 
-    #   assign seat['id']
-    #   reserved << seat['id']
-    #   execute 'INSERT INTO seats_connector (seat_id, service_id, booking_id, ticket_id) VALUES (?,?,?,?)',
-    #           seat['id'], service_id, boocking_id, ticket_id
-    # end
     Service.update_empty_seats service_id
     reserved
-  end
-
-  def self.assign(seat_id)
-    execute('UPDATE seats SET occupied = 1 WHERE id = ?', seat_id)
-  end
-
-  def self.de_assign(seat_id)
-    execute('UPDATE seats SET occupied = 0 WHERE id = ?', seat_id)
   end
 end
