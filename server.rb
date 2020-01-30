56c956c735d7231743f761903421130248f45c11
itggot-adrian-almetunsmeds <adrian.almetunsmeds@elev.ga.ntig.se>
2020-01-30 08:10:21 +0100

    Fixed exception

--- server.rb
+++ server.rb
@@ -35,6 +35,7 @@ class Server < Sinatra::Base # rubocop:disable Metrics/ClassLength
 
   # Displays the edit form for the user
   get '/user/:id/edit' do
+    redirect '/' unless session[:user_id]
     if @user.id == params['id'].to_i
       @users = [@user]
       slim :'templates/user_list'
@@ -73,9 +74,9 @@ class Server < Sinatra::Base # rubocop:disable Metrics/ClassLength
     redirect "/user/#{params['id']}"
   end
 
-  # Displays a user given the id
+  # Displays a user given the id but only if having the rights to do so.
   get '/user/:id' do
-    if session[:user_id] == params[:id] || @admin
+    if session[:user_id].to_i == params[:id].to_i || @admin
       @user = User.fetch_where id: params[:id]
       @bookings = Booking.fetch_where user_id: params[:id]
       slim :'user/overview'
@@ -95,7 +96,7 @@ class Server < Sinatra::Base # rubocop:disable Metrics/ClassLength
   # Removes a booking if the user has the rights to do so.
   get '/booking/:id/delete' do
     @bookings = Booking.fetch_where id: params[:id]
-    redirect Booking.cancel(params[:id], session, params)
+    redirect Booking.cancel(params[:id], session, params, back, @admin)
   end
 
   # Home page
@@ -150,6 +151,8 @@ class Server < Sinatra::Base # rubocop:disable Metrics/ClassLength
   post '/service/:id/update' do
     if @admin
       begin
+        params['departure_time'] = DateTime.parse("#{params['departure_time']}:00+01:00").to_time.to_i
+        params['arrival_time'] = DateTime.parse("#{params['arrival_time']}:00+01:00").to_time.to_i
         Service.new(params).save
       rescue StandardError
         session[:error] = 'Invalid data provided'
@@ -162,12 +165,18 @@ class Server < Sinatra::Base # rubocop:disable Metrics/ClassLength
 
   # Post. Creates a new service or raises an error
   post '/service/create' do
-    if @admin
+    if @admin && params != []
       begin
+        ticket_id = params['ticket_id']
+        params.delete('ticket_id')
         params['departure_time'] = DateTime.parse(params['departure_time']).to_time.to_i
         params['arrival_time'] = DateTime.parse(params['arrival_time']).to_time.to_i
         params['empty_seats'] = Train.fetch_where(id: params['train_id'].to_i).train_type.capacity
-        Service.new(params).save
+        temp = Service.new(params)
+        temp.save(true)
+        ticket_id.flatten.each do |z|
+          Ticket_connector.new(service_id: temp.id, ticket_id: z).save unless z.to_i.negative?
+        end
       rescue StandardError
         session[:error] = 'Invalid data provided'
       end
@@ -235,7 +244,7 @@ class Server < Sinatra::Base # rubocop:disable Metrics/ClassLength
       Booking.create(payload, session, back)
       '/checkout'
     rescue StandardError => e
-      p e.backtrace
+      print e.backtrace
       session[:error_user] = 'Unable to complete booking'
       '/'
     end
@@ -245,6 +254,7 @@ class Server < Sinatra::Base # rubocop:disable Metrics/ClassLength
   # Let's the user confirm the booking
   get '/checkout' do
     @booking = Booking.fetch_where(session_id: session.id.public_id)
+    redirect '/' if @booking == []
     @total_points = 0
     if @booking.is_a? Array
       @booking.each do |booking|
@@ -282,9 +292,15 @@ class Server < Sinatra::Base # rubocop:disable Metrics/ClassLength
   end
 
   # Shows the assigned tickets, with seats and dep/arr times/platforms
+  # Redirects to '/' if not in booking process
   get '/booking-complete' do
+    redirect '/' unless session[:booking_id]
     @booking = Booking.fetch_where id: session[:booking_id]
     @seats = Seat_connector.fetch_where(booking_id: session[:booking_id])
     slim :bookings
+  end
+  not_found do
+    session[:error_user] = 'Page not found'
+    redirect '/'
   end
 end
